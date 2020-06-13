8cc24e51c4223ed289d19a807f7e152524c6a0c1
joachimharms <joachimharms77@gmail.com>
2020-06-13 21:48:36 +0200

    Complete Project LambdaExpressions 1.

--- _05LambdaExpressions/Program.cs
+++ _05LambdaExpressions/Program.cs
@@ -3,12 +3,13 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Security.Authentication;
     using System.Security.Cryptography.X509Certificates;
 
     /// <summary>
     /// Main program of the application.
     /// </summary>
-    internal class Program
+    internal static class Program
     {
         /// <summary>
         /// Main method of the application.
@@ -16,37 +17,56 @@
         /// <param name="args">Default implementation.</param>
         private static void Main(string[] args)
         {
-            ////var cities = new List<string>
-            ////{
-            ////    "London",
-            ////    "Paris",
-            ////    "New York",
-            ////    "Berlin",
-            ////    "Lissabon",
-            ////    "Los Angeles",
-            ////    "Tokio",
-            ////    "Hong Kong",
-            ////    "Madrid",
-            ////    "Helsinki",
-            ////};
+            var cities = new List<string>
+            {
+                "London",
+                "Paris",
+                "New York",
+                "Berlin",
+                "Lissabon",
+                "Los Angeles",
+                "Tokio",
+                "Hong Kong",
+                "Madrid",
+                "Helsinki",
+            };
 
-            ////IEnumerable<string> filteredList =
-            ////    cities.Where(StartsWithL);
+            Console.WriteLine("1.Filtering mit Where mittels benannter Methode:");
+            IEnumerable<string> filteredList =
+                cities.Where(StartsWithL);
 
-            ////foreach (var city in filteredList)
-            ////{
-            ////    Console.WriteLine(city);
-            ////}
+            foreach (var city in filteredList)
+            {
+                Console.WriteLine(city);
+            }
+
+            Console.WriteLine("\n2. Filtering über Where mit anonymer Methode:");
+            IEnumerable<string> filteredCities =
+            cities.Where(delegate (string s) { return StartsWithL(s); });
+
+            foreach (var city in filteredCities)
+            {
+                Console.WriteLine(city);
+            }
+
+            Console.WriteLine("\n3. Filtering mit Lambda Expression Syntax:");
+            IEnumerable<string> filteredListOfCities =
+                cities.Where(s => s.StartsWithL());
+
+            foreach (var city in filteredListOfCities)
+            {
+                Console.WriteLine(city);
+            }
         }
 
-        ////private static bool StartsWithL(string name)
-        ////{
-        ////    // if (name.StartsWith('L'))
-        ////    // {
-        ////    //    return true;
-        ////    // }
-        ////    // return false;
-        ////    return name.StartsWith('L');
-        ////}
+        private static bool StartsWithL(this string name)
+        {
+            // if (name.StartsWith('L'))
+            // {
+            //      return true;
+            // }
+            // return false;
+            return name.StartsWith('L');
+        }
     }
 }
